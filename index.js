bf435267071817579e289fa74f8eabba96d70974
Dalan Lewis <dalanforpresident@gmail.com>
2019-01-24 20:30:39 -0700

    I did some stuff and now things are talking to eachother

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 4000
+let shows = []
 
-app.get('/shows', (req, res) => res.send('Hello World!'))
+app.use(bodyParser.json())
 
-app.post('/shows', (req, res) => res.send('Post Complete'))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    next()
+   })
 
-app.put('/shows', (req, res) => res.send('Putem Up'))
+app.route('/shows')
+.get((req, res) => {
+    
+    
+    res.send(shows)})
 
+
+.post((req, res) => {
+    shows = [...shows, req.body]    
+    res.send(shows)})
+    
+
+.put((req, res) => {res.send('Putem Up')})
 app.delete('/shows', (req, res) => res.send('D-e-l-eted'))
-
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log("i'm listening!"))
