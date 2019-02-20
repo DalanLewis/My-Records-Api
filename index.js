36e29ee4e1a4a1eb883a07b817c11e8d11d10086
Dalan Lewis <dalanforpresident@gmail.com>
2019-02-19 19:19:08 -0700

    wired in the database

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const db = require('monk')('mongodb://admin:password1@ds042688.mlab.com:42688/tv-demo')
+const showsCollection = db.get('shows')
 const app = express()
 const port = 4000
 let shows = []
@@ -10,22 +12,37 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
     res.header('Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization')
     next()
-   })
+})
 
 app.route('/shows')
-.get((req, res) => {
-    
 
-    res.send(shows)})
+    .get(async (req, res) => {
+        try {
+            const shows = await showsCollection.find(req.params._id)
+            res.send(shows)
+        }
+        catch (err) {
+            console.log(err)
+        }
+    })
 
+    .post(async (req, res) => { console.log(req.body)
+        try {
+           await showsCollection.insert(req.body)
+            // shows.push(req.body)
+            // shows = [...shows, req.body]    
+            res.send()
+            console.log(db, shows)
+        }
+        catch (err) {
+            console.log(err)
+        }
+    })
 
-.post((req, res) => {
-    shows = [...shows, req.body]    
-    res.send(shows)})
-    
+    .put((req, res) => { res.send('Putem Up') })
 
-.put((req, res) => {res.send('Putem Up')})
 app.delete('/shows', (req, res) => res.send('D-e-l-eted'))
+
 app.listen(port, () => console.log("i'm listening!"))
